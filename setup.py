9ca5e7f7937ec29959df70e6b2596d6f2b7de3a4
ko-zu <causeless@gmail.com>
2014-11-21 14:21:14 +0900

    Generate description for pypi

--- setup.py
+++ setup.py
@@ -3,6 +3,12 @@
 
 from setuptools import setup, find_packages
 
+try:
+    import pypandoc
+    description = pypandoc.convert('README.md', 'rst')
+except:
+    description = open('README.md').read()
+
 setup(name="publicsuffixlist",
       version="0.1.0",
       packages=["publicsuffixlist",],
@@ -14,11 +20,15 @@ setup(name="publicsuffixlist",
       author="ko-zu",
       author_email="causeless@gmail.com",
       description="publicsuffixlist implement",
+      long_description=description,
       url="https://github.com/ko-zu/psl",
       classifiers=[
           "Development Status :: 3 - Alpha",
           "License :: OSI Approved :: Mozilla Public License 2.0 (MPL 2.0)",
           "Topic :: Internet :: Name Service (DNS)",
+          "Topic :: Text Processing :: Filters",
+          "Operating System :: OS Independent",
+          
         ],
       )
 
