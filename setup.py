83f5cceea8e8c076794e3c8b3fac1b3ae0a413a0
ko-zu <causeless@gmail.com>
2015-06-29 15:28:37 +0900

    Release version 0.2.8

--- setup.py
+++ setup.py
@@ -10,7 +10,7 @@ except:
     description = open('README.md').read()
 
 setup(name="publicsuffixlist",
-      version="0.2.7",
+      version="0.2.8",
       packages=["publicsuffixlist",],
       package_data = {
           "publicsuffixlist": [
