0f862c4b74c7adcd9156e99ae72e7668ce85dbdb
ko-zu <causeless@gmail.com>
2015-09-23 02:03:10 +0900

    Version 0.2.10
    Update PSL.
    Now both sdist and bdist_wheel packages on pypi.

--- setup.py
+++ setup.py
@@ -10,7 +10,7 @@ except:
     description = open('README.md').read()
 
 setup(name="publicsuffixlist",
-      version="0.2.9",
+      version="0.2.10",
       packages=["publicsuffixlist",],
       package_data = {
           "publicsuffixlist": [
