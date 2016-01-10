cfe470ff82cc3a9d9729f2f023ab71ffc3a2379e
ko-zu <causeless@gmail.com>
2016-01-11 01:28:24 +0900

    Update to v0.4.0
    c500524 Merge branch 'meschansky-updatePSL_path_parameter'
    f2745d2 updatePSL: psl_file parameter added

--- setup.py
+++ setup.py
@@ -10,7 +10,7 @@ except:
     description = open('README.md').read()
 
 setup(name="publicsuffixlist",
-      version="0.3.1",
+      version="0.4.0",
       packages=["publicsuffixlist",],
       package_data = {
           "publicsuffixlist": [
