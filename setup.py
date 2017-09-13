f30dd3c79d0fc71af48ec217cb2849fdcf93ab31
ko-zu <causeless@gmail.com>
2017-09-13 18:52:25 +0900

    Update PSL

--- setup.py
+++ setup.py
@@ -10,7 +10,7 @@ except:
     description = open('README.md').read()
 
 setup(name="publicsuffixlist",
-      version="0.4.3",
+      version="0.4.4",
       packages=["publicsuffixlist",],
       package_data = {
           "publicsuffixlist": [
