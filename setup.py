70c4193b4f63be9e1e167c4cb0377db99b4dd796
ko-zu <causeless@gmail.com>
2018-05-18 18:10:28 +0900

    Cleanup pypi descriptions

--- setup.py
+++ setup.py
@@ -2,17 +2,12 @@
 # -*- coding: utf-8 -*-
 
 import codecs
-
 from setuptools import setup
 
-try:
-    import pypandoc
-    description = pypandoc.convert('README.md', 'rst')
-except:
-    description = codecs.open('README.md', encoding='utf-8').read()
+description = codecs.open('README.md', encoding='utf-8').read()
 
 setup(name="publicsuffixlist",
-      version="0.6.0",
+      version="0.6.1",
       packages=["publicsuffixlist"],
       package_data={
           "publicsuffixlist": [
@@ -23,9 +18,10 @@ setup(name="publicsuffixlist",
       author_email="causeless@gmail.com",
       description="publicsuffixlist implement",
       long_description=description,
+      long_description_content_type="text/markdown",
       url="https://github.com/ko-zu/psl",
       classifiers=[
-          "Development Status :: 4 - Beta",
+          "Development Status :: 5 - Production/Stable",
           "License :: OSI Approved :: Mozilla Public License 2.0 (MPL 2.0)",
           "Topic :: Internet :: Name Service (DNS)",
           "Topic :: Text Processing :: Filters",
