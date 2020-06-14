2a0a5c8f51dd7268bfa82618c8ef85728854a8b8
ko-zu <causeless@gmail.com>
2020-06-14 15:46:03 +0900

    Increment version to 0.7.3

--- setup.py
+++ setup.py
@@ -7,7 +7,7 @@ from setuptools import setup
 description = codecs.open('README.md', encoding='utf-8').read()
 
 setup(name="publicsuffixlist",
-      version="0.7.2",
+      version="0.7.3",
       packages=["publicsuffixlist"],
       package_data={
           "publicsuffixlist": [
