17007e226c55e866c8ff9543fe1ed16cfe3e51eb
ko-zu <causeless@gmail.com>
2021-05-30 17:46:58 +0900

    Update PSL

--- setup.py
+++ setup.py
@@ -7,7 +7,7 @@ from setuptools import setup
 description = codecs.open('README.md', encoding='utf-8').read()
 
 setup(name="publicsuffixlist",
-      version="0.7.7",
+      version="0.7.8",
       packages=["publicsuffixlist"],
       package_data={
           "publicsuffixlist": [
